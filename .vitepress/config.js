6889ef01367ffc51c314e5c6c8b71902232382e7
Yi Xin <yixin.coding@qq.com>
2024-05-24 14:14:58 +0800

    feat: 添加搜索框

--- .vitepress/config.js
+++ .vitepress/config.js
@@ -5,6 +5,7 @@ export default {
   srcDir: 'src',
   lang: 'zh-CN',
   lastUpdated: true,
+  cleanUrls: true,
   themeConfig: {
     outlineTitle: '本页目录',
     outline: [2, 4],
@@ -17,6 +18,30 @@ export default {
       '/frontend/': sidebarFrontend(),
       '/ops/': sidebarOps(),
       '/test/': sidebarTest()
+    },
+    search: {
+      provider: 'local',
+      options: {
+        locales: {
+          root: {
+            translations: {
+              button: {
+                buttonText: '搜索文档',
+                buttonAriaLabel: '搜索文档'
+              },
+              modal: {
+                noResultsText: '无法找到相关结果',
+                resetButtonTitle: '清除查询条件',
+                footer: {
+                  selectText: '选择',
+                  navigateText: '切换',
+                  closeText: '关闭'
+                }
+              }
+            }
+          }
+        }
+      }
     }
   },
   sitemap: {
